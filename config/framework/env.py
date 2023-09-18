44bdcc805291866a848055f957f416c8debd014c
cooleydw494 <cooleydw494@gmail.com>
2023-09-18 03:03:36 -0400

    changes to Env and Theme to keep up with codemonkeys core package

--- config/framework/env.py
+++ config/framework/env.py
@@ -1,57 +1,37 @@
-"""==========================================================================================***
-***==  MONKEY MANIFEST  =====================================================================***
-***
-***    The Monkey Manifest houses centralized configuration of automation profiles (monkeys).
-***
-***    Undefined props will default based on `stor/source/defaults/monkey-config-defaults.yaml`.
-***
-***=========================================================================================="""
-
 import os
 from dataclasses import dataclass
 
 from dotenv import load_dotenv
 
-from config.framework.theme import light_mode_enabled, max_terminal_width, verbose_logs_enabled, keywords, text_themes
 from codemonkeys.defs import ROOT_PATH
 
-load_dotenv(os.path.join(ROOT_PATH, '.env'))
+load_dotenv(os.path.join(ROOT_PATH or '.', '.env'))
 
-""" FRAMEWORK_ENV_PROPS_LIST - DO NOT MODIFY
-A List tracking the source's env props. This is currently unused, but could be useful. """
-# [DEFINE_FRAMEWORK_ENV_PROPS_LIST_START]
-required_env_props = ["OPENAI_API_KEY", "CUSTOM_PROP"]
-# [DEFINE_FRAMEWORK_ENV_PROPS_LIST_END]
 
 @dataclass
 class Env:
+    """
+    The Env class provides easy dot notation access to .env vars.
+
+    DO NOT MODIFY the section between [DEFINE_ENV_PROPS_START] and [DEFINE_ENV_PROPS_END].
+    This section is automatically re-generated each time you run a `monk` command.
+    """
+
     _instance = None
 
-    """ FRAMEWORK_ENV_PROPS - DO NOT MODIFY
-    Definitions of the source's env props. These are used to generate the Env class. """
-    # [DEFINE_FRAMEWORK_ENV_PROPS_START]
-    OPENAI_API_KEY: str = os.getenv('OPENAI_API_KEY', 'THIS-ONES-ON-YOU')
-    CUSTOM_PROP: str = os.getenv('CUSTOM_PROP', 'custom_value')
-
-    # [DEFINE_FRAMEWORK_ENV_PROPS_END]
-
-    """ GENERATED ENV PROPERTIES - DO NOT MODIFY
-    Any other properties defined in your .env will be generated and written here on each run of `monk`,
-    enabling full, easy IDE support for any custom .env values. """
-    # [DEFINE_CUSTOM_ENV_PROPS_START]
-
-    # [DEFINE_CUSTOM_ENV_PROPS_END]
-
-    """ THEME - DO NOT MODIFY
-     This should eventually work another way, but for now it's convenient to have it here. """
-    light_mode_enabled = light_mode_enabled
-    max_terminal_width = max_terminal_width
-    verbose_logs_enabled = verbose_logs_enabled
-    keywords = (lambda words: sorted(words, key=len, reverse=True))(keywords)
-    text_themes = text_themes
+    # [DEFINE_ENV_PROPS_START]
+    OPENAI_API_KEY: str = os.getenv('OPENAI_API_KEY')
+    CUSTOM_PROP: str = os.getenv('CUSTOM_PROP')
+    NEW_PROP: int = os.getenv('NEW_PROP')
+    # [DEFINE_ENV_PROPS_END]
 
     @classmethod
-    def get(cls):
+    def get(cls) -> 'Env':
+        """
+        Get the instance of the Env class, or create one if it doesn't exist.
+
+        :return: Singleton instance of the Env class
+        """
         if Env._instance is None:
             Env._instance = Env()
 
