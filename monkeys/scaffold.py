706bee3b4627219a22b0ade7516f5fe3488552d7
cooleydw494 <cooleydw494@gmail.com>
2023-10-16 01:51:40 -0400

    minor changes to scaffolding files

--- monkeys/scaffold.py
+++ monkeys/scaffold.py
@@ -4,9 +4,6 @@ from codemonkeys.defs import STOR_PATH
 
 from monkeys.monkey import Monkey
 
-from codemonkeys.types import OStr
-
-
 @dataclass
 class Scaffold(Monkey):
 
@@ -14,6 +11,10 @@ class Scaffold(Monkey):
     MAIN_PROMPT: str = ("Review the following architectural documentation for a codebase, and write the best "
                         "implementation of the specified file as possible, with close attention to other usable"
                         " elements declared in the architecture overview (classes, functions, etc).")
+
+    MAIN_PROMPT_ULTIMATUM: str = ("Take your time carefully considering architecture information and how it affects"
+                                  " implementation of each file, ensuring among other things, that naming and imports"
+                                  " are correct.")
 
     # Context / Summary
     CONTEXT_FILE_PATH: str = f"{STOR_PATH}/context/scaffold/twitter_poster.txt"
@@ -23,15 +24,15 @@ class Scaffold(Monkey):
 
     # Filepath Extraction
     FILE_EXTRACTION_PROMPT: str = ("Review the following architectural documentation for a codebase and extract a list "
-                                   "of all the filepaths that need to be created to scaffold it. Use absolute paths, "
-                                   f"with the project root dir {PROJECT_ROOT}.")
+                                   "of all the filepaths that need to be created to scaffold it. Always use absolute "
+                                   f"paths, with the project root dir {PROJECT_ROOT} included.")
 
     # Output
-    SKIP_EXISTING_OUTPUT_FILES = True
+    SKIP_EXISTING_OUTPUT_FILES: bool = True
 
     # Git
-    GPT_GIT_COMMITS: bool = False
-    GIT_REPO_PATH: OStr = None
+    GPT_GIT_COMMITS: bool = True
+    GIT_REPO_PATH: str = PROJECT_ROOT
 
     # Models
     MAIN_MODEL: str = 'gpt-4'
